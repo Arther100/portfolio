32dc4a16043d8d5bbea263e4e11bf05f39bc208f
karthikayan.r <karthikayan.r@ciglobalsolutions.com>
2025-01-29 23:13:34 +0530

    contact session edited

--- lib/commons/constants.dart
+++ lib/commons/constants.dart
@@ -1,8 +1,24 @@
 class Constants{
   static const List<String> splashHelloList = ["Hello", "வணக்கம்","नमस्तेे" ,"olá", "سلام"];
   static const List<String> splashHelloImageList = ["assets/11.PNG", "assets/22.PNG","assets/33.PNG" ,"assets/44.PNG", "assets/55.PNG"];
-  static const List<String> logoImageList = ["assets/logo11.PNG", "assets/logo22.PNG", "assets/logo33.PNG", "assets/logo44.jpg", "assets/logo55.PNG", "assets/logo66.PNG", "assets/logo77.PNG", "assets/logo88.PNG"];
-  static const List<String> certificateImageList = ["assets/certificate11.PNG", "assets/certificate22.PNG", "assets/certificate33.PNG", "assets/certificate44.JPG", "assets/ample.gif"];
+  static const List<String> logoImageList = ["assets/logo11.PNG", "assets/logo22.PNG", "assets/logo33.PNG", "assets/logo44.jpg", "assets/logo55.PNG", "assets/logo66.PNG", "assets/logo77.PNG", "assets/logo88.PNG", "assets/logo99.PNG", "assets/logo1010.PNG"];
+  static const List<String> certificateImageList = [
+    "https://karthikayansde.github.io/assets-site/assets/certificate7.png",
+    "https://karthikayansde.github.io/assets-site/assets/certificate2.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate1.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate8.png",
+    "https://karthikayansde.github.io/assets-site/assets/certificate3.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate4.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate9.png",
+    "https://karthikayansde.github.io/assets-site/assets/certificate6.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate5.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/certificate10.png",
+  ];
+  static const List<String> certificateNaanMuthalvan = [
+    "https://karthikayansde.github.io/assets-site/assets/naan1.png",
+    "https://karthikayansde.github.io/assets-site/assets/naan2.jpg",
+    "https://karthikayansde.github.io/assets-site/assets/naan1.png",
+  ];
   static const String font = 'MyFont';
   static const String linkLinkedIn = 'https://www.linkedin.com/in/karthikayansde/';
   static const String linkGit = 'https://github.com/karthikayansde';
--- lib/services/functional_services.dart
+++ lib/services/functional_services.dart
@@ -4,6 +4,7 @@ import 'package:karthi_kayan_portfolio/commons/strings.dart';
 import 'package:karthi_kayan_portfolio/services/copy_paste_service.dart';
 import 'package:karthi_kayan_portfolio/services/url_launcher.dart';
 import 'package:karthi_kayan_portfolio/widgets/text_widget.dart';
+import 'package:photo_view/photo_view.dart';
 
 class FunctionalServices{
   static Future<void> launchAndCopyUrl({required BuildContext context, required String URL, String? textToCopy}) async {
@@ -19,4 +20,42 @@ class FunctionalServices{
     },
     );
 }
+
+  static Widget showImage(String source, BuildContext context) {
+    return AlertDialog(
+        contentPadding: EdgeInsets.all(0),
+        backgroundColor: MyColors.white,
+        content: AspectRatio(
+          aspectRatio: 3 / 2,
+          child: Stack(
+            children: [
+              PhotoView(
+                imageProvider: NetworkImage(source),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      color: MyColors.black,
+                      child: Center(
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
 }
